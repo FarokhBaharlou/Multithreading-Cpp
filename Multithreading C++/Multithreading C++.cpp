5fe935d1eeed364e114dfe33df05782ee9ce87a3
Farokh <farokhbahalou@yahoo.com>
2022-06-14 15:37:08 -0400

    Generating some random datasets and measuring its time

--- Multithreading C++/Multithreading C++.cpp
+++ Multithreading C++/Multithreading C++.cpp
@@ -1,7 +1,26 @@
 
 #include <iostream>
+#include <vector>
+#include <array>
+#include <random>
+#include <ranges>
+#include "Timer.h"
+
+constexpr size_t DATASET_SIZE = 5000000;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::minstd_rand rne;
+    std::vector<std::array<int, DATASET_SIZE>> datasets{ 4 };
+    Timer timer;
+
+    timer.Mark();
+    for (auto& arr : datasets)
+    {
+        std::ranges::generate(arr, rne);
+    }
+    auto t = timer.Peek();
+
+    std::cout << "Generating the datasets took " << t << " seconds" << std::endl;
+    return 0;
 }
