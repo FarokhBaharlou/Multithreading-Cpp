af2dcd4d7e3af7ff52f7c97e5636dd84e042a00a
Farokh <farokhbahalou@yahoo.com>
2022-06-14 16:05:37 -0400

    doing some processing on our dataset and measuring the time

--- Multithreading C++/Multithreading C++.cpp
+++ Multithreading C++/Multithreading C++.cpp
@@ -4,6 +4,8 @@
 #include <array>
 #include <random>
 #include <ranges>
+#include <cmath>
+#include <limits>
 #include "Timer.h"
 
 constexpr size_t DATASET_SIZE = 5000000;
@@ -14,13 +16,23 @@ int main()
     std::vector<std::array<int, DATASET_SIZE>> datasets{ 4 };
     Timer timer;
 
-    timer.Mark();
-    for (auto& arr : datasets)
+    for (auto& set : datasets)
     {
-        std::ranges::generate(arr, rne);
+        std::ranges::generate(set, rne);
+    }
+
+    timer.Mark();
+    for (auto& set : datasets)
+    {
+        for (int num : set)
+        {
+            constexpr auto limit = (double)std::numeric_limits<int>::max();
+            const auto x = (double)num / limit;
+            set[0] += int(std::sin(std::cos(x)) * limit);
+        }
     }
     auto t = timer.Peek();
 
-    std::cout << "Generating the datasets took " << t << " seconds" << std::endl;
+    std::cout << "Processing the datasets took " << t << " seconds" << std::endl;
     return 0;
 }
