eab65693ae4f1efa19f23bdb959a3e6fb7ac330a
Farokh <farokhbahalou@yahoo.com>
2022-06-14 23:05:52 -0400

    using mutex for synchronization in this race condition(makes program slower in this situation)

--- Multithreading C++/Multithreading C++.cpp
+++ Multithreading C++/Multithreading C++.cpp
@@ -7,14 +7,17 @@
 #include <cmath>
 #include <limits>
 #include <thread>
+#include <mutex>
 #include "Timer.h"
 
 constexpr size_t DATASET_SIZE = 5000000;
 
-void ProcessDataset(std::array<int, DATASET_SIZE>& set, int& sum)
+void ProcessDataset(std::array<int, DATASET_SIZE>& set, int& sum, std::mutex& mtx)
 {
     for (int num : set)
     {
+        //Locking and unlocking mutex has overhead which is not good for this situation of our large dataset
+        std::lock_guard g{ mtx };
         constexpr auto limit = (double)std::numeric_limits<int>::max();
         const auto x = (double)num / limit;
         sum += int(std::sin(std::cos(x)) * limit);
@@ -33,12 +36,13 @@ int main()
         std::ranges::generate(set, rne);
     }
 
+    std::mutex mtx;
     int sum = 0;
 
     timer.Mark();
     for (auto& set : datasets)
     {
-        workers.push_back(std::thread{ ProcessDataset, std::ref(set), std::ref(sum) });
+        workers.push_back(std::thread{ ProcessDataset, std::ref(set), std::ref(sum), std::ref(mtx) });
     }
 
     for (auto& worker : workers)
